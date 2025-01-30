20fa448dc42165f2bc0634c403c5eb0aa3d96093
elicaro <carolinatolosa02@gmail.com>
2025-01-29 21:27:27 -0500

    Update DemoTest3.java

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -11,6 +11,20 @@ public class DemoTest3 {
 		int result = a + b;
 		
 		System.out.println(result);
+
+
+             int x = 46;
+	     int y = 32;
+
+		if(x > y){
+			System.out.println("Greater");
+		}else{
+			System.out.println("Less");
+		}
+
+	// This is coming from github
+	// practing pulling
+		
 		
 	}
 	

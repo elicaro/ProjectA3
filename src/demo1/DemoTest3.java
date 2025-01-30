ec8adc5e396b617164c32ce6e0e99ac99bd29de9
carol <carol@10.0.0.241>
2025-01-29 21:08:31 -0500

    Implemented code for Java and Git

--- src/demo1/DemoTest3.java
+++ src/demo1/DemoTest3.java
@@ -3,10 +3,23 @@ package demo1;
 public class DemoTest3 {
 	
 	public static void main (String args[]) {
-		System.out.println("Java");
+		System.out.println("Implement");
+		
+		int a = 5;
+		int b = 3;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
 	
 	}
 	
 	
 
-}
+
